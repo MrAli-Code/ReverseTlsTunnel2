2ebb54021915f1efe8cfe467b673809a9f0f4d30
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-22 13:51:37 +0330

    update RtTunnel.sh

--- RtTunnel.sh
+++ RtTunnel.sh
@@ -59,14 +59,22 @@ configure_arguments() {
     read -p "Which server do you want to use? (Enter '1' for Iran or '2' for Kharej) : " server_choice
     read -p "Please Enter SNI (default : splus.ir): " sni
     sni=${sni:-splus.ir}
+    read -p "Do you want to use mux? (yes/no): " use_mux
+    mux_width=2
+    if [ "$use_mux" == "yes" ]; then
+        read -p "Enter mux-width (default: 2): " mux_width
+        mux_width=${mux_width:-2}
+    else
+        mux_width=1
+    fi
 
     if [ "$server_choice" == "2" ]; then
         read -p "Please Enter (IRAN IP) : " server_ip
         read -p "Please Enter Password (Please choose the same password on both servers): " password
-        arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni --terminate:24"
+        arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni --mux-width:$mux_width --terminate:24"
     elif [ "$server_choice" == "1" ]; then
         read -p "Please Enter Password (Please choose the same password on both servers): " password
-        arguments="--iran --lport:23-65535 --sni:$sni --password:$password --terminate:24"
+        arguments="--iran --lport:23-65535 --sni:$sni --password:$password --mux-width:$mux_width --terminate:24"
     else
         echo "Invalid choice. Please enter '1' or '2'."
         exit 1
@@ -118,6 +126,14 @@ configure_arguments2() {
     read -p "Which server do you want to use? (Enter '1' for Iran or '2' for Kharej) : " server_choice
     read -p "Please Enter SNI (default : splus.ir): " sni
     sni=${sni:-splus.ir}
+    read -p "Do you want to use mux? (yes/no): " use_mux
+    mux_width=2
+    if [ "$use_mux" == "yes" ]; then
+        read -p "Enter mux-width (default: 2): " mux_width
+        mux_width=${mux_width:-2}
+    else
+        mux_width=1
+    fi
 
     if [ "$server_choice" == "2" ]; then
         read -p "Is this your main server (VPN server)? (yes/no): " is_main_server
@@ -125,10 +141,10 @@ configure_arguments2() {
         read -p "Please Enter Password (Please choose the same password on both servers): " password
 
         if [ "$is_main_server" == "yes" ]; then
-            arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni --terminate:24"
+            arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni --mux-width:$mux_width --terminate:24"
         elif [ "$is_main_server" == "no" ]; then
             read -p "Enter your main IP (VPN Server):  " main_ip
-            arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:$main_ip --toport:multiport --password:$password --sni:$sni --terminate:24"
+            arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:$main_ip --toport:multiport --password:$password --sni:$sni --mux-width:$mux_width --terminate:24"
         else
             echo "Invalid choice for main server. Please enter 'yes' or 'no'."
             exit 1
@@ -136,7 +152,7 @@ configure_arguments2() {
 
     elif [ "$server_choice" == "1" ]; then
         read -p "Please Enter Password (Please choose the same password on both servers): " password
-        arguments="--iran --lport:23-65535 --password:$password --sni:$sni --terminate:24"
+        arguments="--iran --lport:23-65535 --password:$password --sni:$sni --mux-width:$mux_width --terminate:24"
         
         num_ips=0
         while true; do
